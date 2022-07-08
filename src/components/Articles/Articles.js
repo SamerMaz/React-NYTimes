f47e583cb3f7e5e8da707fee5d5e4e1f1686bb18
samer.mazraani <samer.mazraani96@gmail.com>
2022-07-08 22:48:45 +0300

    implement infinite scroll using useInfinitquery

--- src/components/Articles/Articles.js
+++ src/components/Articles/Articles.js
@@ -1,35 +1,73 @@
 import { Grid, TextField, Toolbar } from "@material-ui/core";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import useStyles from "./styles";
 import PropTypes from "prop-types";
 import Article from "./Article/Article";
+import { getArticles } from "../../services/service";
 
-const Articles = ({ loading, articles }) => {
+import { useQuery,useInfiniteQuery } from "react-query";
+import axios from "axios";
+
+
+
+const Articles = () => {
+  const totalPages=100
   const classes = useStyles();
+  // const [pageNum, setPageNum] = useState(0);
 
-  return (
+  // const {isLoading, data }= useQuery('articles', ()=>getArticles(pageNum))
+  const { isLoading, data, hasNextPage, fetchNextPage } = useInfiniteQuery('articles', 
+      ({pageParam=1})=> getArticles(pageParam), {
+        getNextPageParam: (lastPage, allPages)=> {
+          const maxPages = lastPage.length * totalPages / 10;
+          console.log("LastPage",maxPages)
+          const nextPage = allPages.length+1;
+          return nextPage <= maxPages ? nextPage : undefined
+        }
+      })
+      // console.log(data)
+
+
+      useEffect(()=>{
+        let fetching= false;
+        const onScroll = async(event)=>{
+          const {scrollHeight, scrollTop, clientHeight } = event.target.scrollingElement;
+
+          if(!fetching && scrollHeight - scrollTop <=clientHeight * 1.5){
+            fetching = true;
+            if(hasNextPage) await fetchNextPage();
+            fetching= false;
+          }
+        };
+
+        document.addEventListener('scroll', onScroll);
+        return ()=>{
+          document.removeEventListener('scroll', onScroll)
+        }
+
+      }, [])
+
+  return(
     <>
-      {loading ? (
+     
+      {isLoading ? (
         "Loading..."
       ) : (
         <div className={classes.root}>
           <Grid container spacing={3}>
-            {articles.map((article) => (
+          {data.pages.map((page) => (
+            page.map((article) => (
               <Grid item xs={12} sm={4} key={article._id}>
                 <Article article={article} />
                 {/* {console.log(article)} */}
               </Grid>
-            ))}
+            ))))}
           </Grid>
         </div>
       )}
-    </>
-  );
-};
-
-Articles.propTypes = {
-  loading: PropTypes.bool.isRequired,
-  articles: PropTypes.array.isRequired,
-};
+    
+  </>
+  )
+}
 
 export default Articles;
